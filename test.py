947a8fbfa1b21008400459e3de40b1a5a3fcd5fb
Matt Thomas <matt@betweenbrain.com>
2015-03-12 16:21:06 -0400

    Adds exmaple code from http://docs.travis-ci.com/user/gui-and-headless-browsers/ for running on Sauce labs

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# WebDriver
+username = os.environ["betweenbrain"]
+access_key = os.environ["e2007f7b-afd4-43a3-af7f-c5087c82199a"]
+capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+hub_url = "%s:%s@localhost:4445" % (username, access_key)
+driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
