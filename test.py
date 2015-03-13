87bc24b05f097f7cdfbabb390e4a73e3cd5b00ba
Matt Thomas <matt@betweenbrain.com>
2015-03-13 18:59:43 -0400

    Try Python SauceClient to report test results back to Sauce Labs again

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from sauceclient import SauceClient
 
 # Start Saucelabs connection
 sauce_url = "http://betweenbrain:e2007f7b-afd4-43a3-af7f-c5087c82199a@ondemand.saucelabs.com:80/wd/hub"
@@ -36,3 +37,7 @@ try:
 
 finally:
     driver.quit()
+
+# Report test results back to Sauce Labs
+sauce_client = SauceClient("betweenbrain", "e2007f7b-afd4-43a3-af7f-c5087c82199a")
+sauce_client.jobs.update_job(driver.session_id, passed=True)
