5c60d5acfa43110e8f04d7314ef2e923f0fa691c
Matt Thomas <matt@betweenbrain.com>
2015-03-12 20:40:03 -0400

    Adds Python integration code from https://docs.saucelabs.com/tutorials/python/

--- test.py
+++ test.py
@@ -4,15 +4,19 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# WebDriver
-username = os.environ["betweenbrain"]
-access_key = os.environ["e2007f7b-afd4-43a3-af7f-c5087c82199a"]
-capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
-hub_url = "%s:%s@localhost:4445" % (username, access_key)
-driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
-
 # Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
+sauce_url = "http://betweenbrain:e2007f7b-afd4-43a3-af7f-c5087c82199a@ondemand.saucelabs.com:80/wd/hub"
+
+desired_capabilities = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': "31",
+}
+
+driver = webdriver.Remote(desired_capabilities=desired_capabilities,
+                          command_executor=sauce_url)
+driver.implicitly_wait(10)
+# End Saucelabs
 
 # Navigate to http://espn.go.com/
 driver.get("http://espn.go.com/")
@@ -22,9 +26,6 @@ inputElement = driver.find_element_by_name("&lpos=sitenavdefault&lid=sitenav_nfl
 
 # Click the found link
 inputElement.click()
-
-# Print the page title:
-print driver.title
 
 try:
     # we have to wait for the page to refresh, the last thing that seems to be updated is the title
